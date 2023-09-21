7e961af74c499c896f567886de5e40e4909c8f45
路星辞 <yangpmldl@163.com>
2023-09-21 21:11:10 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { hopeTheme } from "vuepress-theme-hope";
 
 export default defineUserConfig({
   base: "/",
@@ -8,8 +9,9 @@ export default defineUserConfig({
   title: "代码之道",
   description: "文艺青年的代码分享平台",
 
-  theme,
-
+  theme: hopeTheme({
+    logo: "/logo.png",
+  }),
   // Enable it with pwa
   // shouldPrefetch: false,
 });
